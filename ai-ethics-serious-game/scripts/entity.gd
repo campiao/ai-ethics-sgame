6e1538f22c34d95ec4eca6a3ba91941f055f69cd
campiao <pedro.campiao1@gmail.com>
2025-03-28 23:26:30 +0000

    feat: add ability to show text like typewritter

--- ai-ethics-serious-game/scripts/entity.gd
+++ ai-ethics-serious-game/scripts/entity.gd
@@ -3,6 +3,12 @@ class_name EntityType
 
 @export var entity_name : String = ""
 
+@export_group("Question related properties")
 @export_multiline var question_text : String = ""
 @export_multiline var answer_text : String = ""
 @export var is_answer_correct : bool = false
+
+@export_group("Post-Question properties")
+@export_multiline var explanation_text : String = ""
+@export_multiline var correct_choice_text : String = ""
+@export_multiline var wrong_choice_text : String = ""
--- ai-ethics-serious-game/scripts/level_one/world_manager.gd
+++ ai-ethics-serious-game/scripts/level_one/world_manager.gd
@@ -15,10 +15,22 @@ extends Node2D
 @onready var entities: Node2D = $Entities
 @onready var entities_remaining := entities.get_children()
 
+# Boss entity
+@onready var boss_entity: EntityType = $BossEntity
+
+# Buttons
+@onready var accept_button: TextureButton = $UI/AcceptButton
+@onready var decline_button: TextureButton = $UI/DeclineButton
+
+
 var current_entity_id := -1
 var entities_count := -1
 
 var is_entity_visible := false
+var is_tutorial_done := false
+var is_on_after_selection := true
+
+var text_update_speed := 2.0
 
 func _ready() -> void:
 	#var first_entity := entities_remaining[0]
@@ -28,10 +40,25 @@ func _ready() -> void:
 	
 	progress_label.text = "Progress: " + str(current_entity_id) + "/" + \
 						str(entities_count)
+						
+	answer_label.visible_ratio = 0.0
+	question_label.visible_ratio = 0.0
 	
 func _process(_delta: float) -> void:
 	if not is_entity_visible:
-		setup_entity(entities_remaining[current_entity_id])
+		if is_tutorial_done:
+			setup_entity(entities_remaining[current_entity_id])
+		#setup_entity(boss_entity)
+		# TODO: Add screen with boss button
+		is_tutorial_done = true
+	else:
+		if question_label.visible_ratio < 1.0:
+			question_label.visible_ratio += \
+			1.0/question_label.text.length()/text_update_speed
+		elif answer_label.visible_ratio < 1.0:
+			answer_label.visible_ratio += \
+			1.0/answer_label.text.length()/text_update_speed
+	
 	
 func  setup_entity(entity_data: EntityType) -> void:
 	# Display entity data
@@ -45,13 +72,43 @@ func  setup_entity(entity_data: EntityType) -> void:
 	is_entity_visible = true
 
 func _on_accept_button_pressed() -> void:
+	if current_entity_id > entities_count-1:
+		return
 	# TODO: Logic for pressing accept button
-	advance_level()
+	# TODO: Fix after selection boss interaction
+	# It kinda works but is currently using the accept button
+	answer_label.visible_ratio = 0.0
+	question_label.visible_ratio = 0.0
+	if not is_on_after_selection:
+		is_on_after_selection = true
+		advance_level()
+	else:
+		var is_choice_correct = true \
+		if entities_remaining[current_entity_id].is_answer_correct \
+		else false
+		setup_boss_text(entities_remaining[current_entity_id], is_choice_correct)
+	
 	
 func _on_decline_button_pressed() -> void:
+	if current_entity_id > entities_count - 1:
+		return
 	# TODO: Logic for pressing decline button
+	answer_label.visible_ratio = 0.0
+	question_label.visible_ratio = 0.0
 	advance_level()
 	
+	
+func setup_boss_text(entity_data: EntityType,
+	is_choice_correct: bool) -> void:
+	boss_entity.answer_text = entity_data.explanation_text
+	# TODO: Make it so the question text depends on if the player choose the
+	# right option.
+	boss_entity.question_text = entity_data.correct_choice_text \
+		if is_choice_correct \
+		else entity_data.wrong_choice_text
+	setup_entity(boss_entity)
+	is_on_after_selection = false
+
 func advance_level() -> void:
 	current_entity_id += 1
 	progress_label.text = "Progress: " + str(current_entity_id) + "/" + \
